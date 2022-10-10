b744349ca0431d464f245005744c24f17edbaacc
Javier18121998 <Javier.Marin.Garcia.Linkedin01@hotmail.com>
2022-10-10 17:34:13 -0500

    now

---
+++ appjs/navigation.js
@@ -0,0 +1,26 @@
+
+window.onload = function(){
+    navigate();
+}
+const navigate = () =>{
+    const start = document.getElementById('start_page');
+    const ours = document.getElementById('ours_page');
+    const products = document.getElementById('products_page');
+    const contacts = document.getElementById('contacts_page');
+    function valery(){
+        start.addEventListener('click', function(){
+            location.href = "../index.html";
+        });
+        ours.addEventListener('click', function(){
+            location.href = "../pages/nosotros.html";
+        });
+        products.addEventListener('click', function(){
+            location.href = "../pages/productos.html";
+        });
+        contacts.addEventListener('click', function(){
+            location.href = "../pages/contacto.html";
+        });
+    }
+    valery();
+    console.log("hola wey");
+}
